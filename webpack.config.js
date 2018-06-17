9010832155bbc4ee7f1ef89a3a1833519da79029
zhaopeng16 <zhaopeng16@meituan.com>
2018-06-17 11:20:19 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: 'css-loader'
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
